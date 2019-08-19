5140bee2e8e9412f4500214fe0973c0a8f2f6646
Ellie <elliemmanifold@gmail.com>
2019-08-19 11:48:33 +0000

    replace mongod for mongoDB Atlas

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ app.listen(3000, function() {
 	console.log('YelpCamp server started');
 });
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://EManifold:Pollyfrosty123@cluster0-lbblu.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!')
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
